bf2c887cbc9b71221715142d73a0e219cce4d7d4
isolderea <ionut33bs@yahoo.com>
2021-01-31 15:01:21 +0100

    Drag and drop not yet done
    Drag and drop not yet done

--- src/test/java/Recipes/DragAndDropTest.java
+++ src/test/java/Recipes/DragAndDropTest.java
@@ -2,18 +2,18 @@ package Recipes;
 
 import base.BaseTest;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class DragAndDropTest extends BaseTest {
 
     @Test
     public void dragAndDropTest() {
-
+    //THIS IS NOT WORKING YET
         //navigate to the Page
         driver.get("https://the-internet.herokuapp.com/drag_and_drop");
 
@@ -22,37 +22,35 @@ public class DragAndDropTest extends BaseTest {
         WebElement destinationObj = driver.findElement(By.id("column-b"));
 
         /* Do the action. */
-        dragDrop(sourceObj,destinationObj);
+        dragIT(sourceObj,destinationObj);
 
         /* Verify the drag and drop action success. */
         String dropStatusText = destinationObj.getText();
-        Assert.assertEquals(dropStatusText, "Dropped!");
+        Assert.assertEquals(dropStatusText, "A");
     }
 
 
     /* This method implement the drag and drop action use selenium webdriver. */
-    public void dragDrop(WebElement srcElement, WebElement targetElement) {
-        try {
-            /* First ensure both source web element and target web element are all displayed. */
-            if (srcElement.isDisplayed() && targetElement.isDisplayed()) {
+    public void dragIT (WebElement From, WebElement To){
+        System.out.println("drag and drop started");
 
-                /* Create the actions object which carry out the drag and drop action. */
-                Actions actionObj = new Actions(this.driver);
+        int x = To.getLocation().x;
+        int y = To.getLocation().y;
 
-                /* Drag source web element and Drop it to the destination. */
-                actionObj.dragAndDrop(srcElement, targetElement).build().perform();
-            } else {
-                System.out.println("Please make sure the web element is displayed for drag and drop. ");
-            }
-        } catch (StaleElementReferenceException ex) {
-            /* This exception means the source or target web element has be changed (delete or updated) in the page DOM tree. */
-            ex.printStackTrace();
-        } catch (NoSuchElementException ex) {
-            /* This exception means can not find the source or target web element in the web page. */
-            ex.printStackTrace();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        Actions actions = new Actions(driver);
+        actions.moveToElement(From)
+                .pause(Duration.ofSeconds(1))
+                .clickAndHold(From)
+                .pause(Duration.ofSeconds(1))
+                .moveByOffset(x, y)
+                .moveToElement(To)
+                .moveByOffset(x,y)
+                .pause(Duration.ofSeconds(1))
+                .release().build().perform();
+
+        System.out.println("drag and drop ended");
+
+
     }
 
 
